4154419992ddd043daa2f728034f8a635f183342
golesmaryjoy33-code <golesmaryjoy33@gmail.com>
2026-04-21 06:45:51 +0000

    input function

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
-let subscription = [];
+let subscriptions = [];
 
 function addSubscription(){
 
-    
+    function addSubsciption (){
+        const name= document.getElementById("name").ariaValueMax;
+        const cost= document.getElementById("cost").ariaValueMax;
+        const cycle= document.getElementById("cycle").ariaValueMax;
+        const used= document.getElementById("used").ariaValueMax;
+
+
+if (!name || isNan(cost)){
+    alert ("Please fill all fields!");
+    return;
+}
+
+//normalize the cost to monthly
+let monthlyCost = cycle === "yearly"? cost / 12: cost;
+
+subscriptions.push({
+    name,
+    monthlyCost,
+    used
+
+
+});
+ render();
+ clearForm();
+    }
 }
